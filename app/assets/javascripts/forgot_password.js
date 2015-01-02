0399f36ab5055abd42b0bc30dde816fe9f3bb0ec
Haseeb Ahmad <engg.haseeb@gmail.com>
2015-01-02 20:28:11 +0500

    authentication cycle review

--- app/assets/javascripts/forgot_password.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.forgot_password').click(function() {
-        $("#form-login").css("display", "none");
-        $("#forget_password").css("display", "block");
-    });
-
-  })
