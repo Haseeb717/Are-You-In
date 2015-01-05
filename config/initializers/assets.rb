140bb0db2ddeb1e393f51c44dfe799ea4ef239e1
Haseeb Ahmad <engg.haseeb@gmail.com>
2015-01-05 13:00:15 +0500

    assets changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 Rails.application.config.assets.precompile += %w(landing.css)
 Rails.application.config.assets.precompile += %w(landing.js)
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
