d7945edfa4a3087db44236ddc4225afcdb1c8e87
Haseeb Ahmad <engg.haseeb@gmail.com>
2015-01-06 18:11:49 +0500

    model trim

--- app/models/team.rb
+++ app/models/team.rb
@@ -11,9 +11,9 @@ class Team < ActiveRecord::Base
 
 	before_save do
 		self.age = self.age.downcase if self.age
-		self.name = self.age.name if self.name
-		self.sport = self.age.sport if self.sport
-		self.city = self.age.city if self.city
+		self.name = self.name if self.name
+		self.sport = self.sport if self.sport
+		self.city = self.city if self.city
 		self.gender = self.gender.downcase if self.gender
 
 		self.age_to = self.age_from = nil unless self.age == "youth"
