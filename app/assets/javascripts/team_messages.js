f8987502f1e006088c2a0e5f34d965860bfd228b
Haseeb Ahmad <engg.haseeb@gmail.com>
2015-01-19 16:30:38 +0500

    working on team talk

--- app/assets/javascripts/team_messages.js
+++ app/assets/javascripts/team_messages.js
@@ -9,23 +9,26 @@ $(document).ready(function() {
 			text = $(this).val().trim("");
 			parent = "";
 			reply = "";
+			page = "";
 
 			// confirming complete message
 			if (team == undefined || team == null || user == undefined ||
 					user == null || text == undefined || text == null || text == "")
 				return;
 
-			submitTeamMessage(team, user, text, parent, reply);
+			$(this).attr("disabled", "disabled");
+			submitTeamMessage(team, user, text, parent, reply, page);
 		}
 	});
 
 	// user reply to specific message handler
 	$(document).on("click", ".btn-reply", function(event) {
-		team = $("#team_id").val();
+		team = $(this).siblings(".team").val();
 		user = $("#user_id").val();
 		text = $(this).siblings("textarea").val().trim("");
 		parent = $(this).siblings(".parent-message").val();
 		reply = $(this).siblings(".reply-user").val();
+		page = (window.location.href.indexOf("dashboard") > 0) ? "dashboard" : "";
 
 		// confirming complete reply message
 		if (team == undefined || team == null || user == undefined ||
@@ -34,17 +37,18 @@ $(document).ready(function() {
 				reply == undefined || reply == null || reply == "")
 			return;
 
-		submitTeamMessage(team, user, text, parent, reply);
+		$(this).attr("disabled", "disabled");
+		submitTeamMessage(team, user, text, parent, reply, page);
 	});
 
 	// user reply all handler
 	$(document).on("click", ".btn-reply-all", function(event) {
-		team = $("#team_id").val();
+		team = $(this).siblings(".team").val();
 		user = $("#user_id").val();
 		text = $(this).siblings("textarea").val().trim("");
 		parent = $(this).siblings(".parent-message").val();
 		reply = "";
-
+		page = (window.location.href.indexOf("dashboard") > 0) ? "dashboard" : "";
 
 		// confirming complete reply message
 		if (team == undefined || team == null || user == undefined ||
@@ -52,13 +56,15 @@ $(document).ready(function() {
 				parent == undefined || parent == null || parent == "")
 			return;
 
-		submitTeamMessage(team, user, text, parent, reply);
+		$(this).attr("disabled", "disabled");
+		submitTeamMessage(team, user, text, parent, reply, page);
 	});
 
-	function submitTeamMessage(team, user, text, parent, reply) {
+	function submitTeamMessage(team, user, text, parent, reply, page) {
 		postData = "user_id=" + user + "&text=" + text + "&parent=" + parent + "&re";
-		postData = "user_id=USER&text=TEXT&parent=PARENT&reply_to=REPLY".replace("USER", user)
-					.replace("TEXT", text).replace("PARENT", parent).replace("REPLY", reply);
+		postData = "user_id=USER&text=TEXT&parent=PARENT&reply_to=REPLY&page=PAGE".replace("USER", user)
+					.replace("TEXT", text).replace("PARENT", parent).replace("REPLY", reply)
+					.replace("PAGE", page);
 
 		$.ajax({
 			type: "POST",
@@ -68,7 +74,6 @@ $(document).ready(function() {
 			success: function (data) {
 				// console.log(data);
 				$(".team-feed-widget").html(data.design);
-
 				shortifyFeedText();
 			},
 			error: function(XMLHttpRequest, textStatus, errorThrown) {
@@ -79,7 +84,6 @@ $(document).ready(function() {
 
 	$(document).on("click", ".reload-team-feeds", function(event) {
 		event.preventDefault();
-		
 		team = $("#team_id").val();
 
 		// team id required for update
@@ -93,7 +97,6 @@ $(document).ready(function() {
 			success: function (data) {
 				// console.log(data);
 				$(".team-feed-widget").html(data.design);
-
 				shortifyFeedText();
 			},
 			error: function(XMLHttpRequest, textStatus, errorThrown) {
