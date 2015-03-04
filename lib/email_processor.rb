efaf641ced65066cfafe14ff5f461a7166b42dab
Haseeb Ahmmad <engg.haseeb@gmail.com>
2015-03-05 04:40:39 +0500

    griddler testing 4

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -5,8 +5,13 @@ class EmailProcessor
 	end	
 	
 	def process
+		puts "abc"
 		puts "email raw html"+@email.raw_html
 		puts "email from"+@email.from[:email]
-		puts "email raw body"+@email.raw_body    
+		puts "email raw body"+@email.raw_body 
+
+		# thread  = TeamMessage.where(id parent nil ) || TeamMessage.find(id).parent 
+
+
 	end
 end
