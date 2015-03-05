290403f8d4a90fdca8e0bf294ea40fce7f0e3a12
Haseeb Ahmmad <engg.haseeb@gmail.com>
2015-03-05 05:48:16 +0500

    4

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -5,12 +5,11 @@ class EmailProcessor
 	end	
 	
 	def process
+		puts @email.body
+		puts @email.from[:email]
 		raw_html = @email.raw_html
-		puts "abc"
 		id = Nokogiri::HTML(raw_html).xpath('//input[name="parent_id"').first
 		puts "id is"+" "+id
-		from = @email.from[:email]
-		puts @email.body
 		# thread  = TeamMessage.where(id parent nil ) || TeamMessage.find(id).parent 
 	end
 end
