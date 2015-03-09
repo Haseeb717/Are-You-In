d31f5baaa3fd2af57e626692ea2aa4c174520de1
Haseeb Ahmmad <engg.haseeb@gmail.com>
2015-03-09 08:37:18 +0500

    email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -13,7 +13,7 @@ class UserMailer < ActionMailer::Base
 		@message = message
 		@user = user
 		dummy_address = "notification+all@are-you.in"
-		mail(:to => "#{user.name} <#{user.email}>", :subject => "#{message.team.name.titleize}: #{message.user.name.titleize} has posted new comment.")
+		mail(:to => "#{user.name} <#{user.email}>,<#{dummy_address}>", :subject => "#{message.team.name.titleize}: #{message.user.name.titleize} has posted new comment.")
 	end
 
 	def reply_message_notification(message, user)
