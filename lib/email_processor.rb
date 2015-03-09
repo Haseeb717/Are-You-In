8d60e5bd10253002d63383228dd19090dc250ca6
Haseeb Ahmmad <engg.haseeb@gmail.com>
2015-03-09 07:30:52 +0500

    email

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -11,41 +11,46 @@ class EmailProcessor
 
 		 	text =  @email.body
 		 	from = @email.from[:email]
-		 	replies_all = @email.cc
-		 	raw_html = @email.raw_html
-		 	team_message_id = Nokogiri::HTML(raw_html).xpath("//input[@name='parent_id']").first.attr("value")
-		 	team_message = TeamMessage.find(team_message_id)
-		 	parent = team_message.parent || team_message
-		 	team = team_message.team
-		 	user = team.users.where(:email => from).first
+		 	reply_cc = @email.cc
+		 	reply_to = @email.to
+		 	puts "reply_to" reply_to.count
+		 	reply_bcc = @email.bcc
+		 	puts "reply_bcc #{reply_bcc}"
+		 	puts "#{@email}"
+		 # 	raw_html = @email.raw_html
+		 # 	team_message_id = Nokogiri::HTML(raw_html).xpath("//input[@name='parent_id']").first.attr("value")
+		 # 	team_message = TeamMessage.find(team_message_id)
+		 # 	parent = team_message.parent || team_message
+		 # 	team = team_message.team
+		 # 	user = team.users.where(:email => from).first
 
-			if team_message and user
-				# creating new message
-				message = TeamMessage.new(:text => text)
+			# if team_message and user
+			# 	# creating new message
+			# 	message = TeamMessage.new(:text => text)
 
-				# associate with team and respective user
-				team.team_messages << message
-				user.team_messages << message
+			# 	# associate with team and respective user
+			# 	team.team_messages << message
+			# 	user.team_messages << message
 
-				# managing reply
-				message.parent = parent
+			# 	# managing reply
+			# 	message.parent = parent
 				
-				message.save!
-				puts "message save"
-				if replies_all.empty?
-					puts "reply"
-					UserMailer.reply_message_notification(message, parent.user).deliver! if user.allow_email and parent
+			# 	message.save!
+			# 	puts "message save"
+			# 	if reply_cc.empty?
+			# 		puts "reply"
+			# 		UserMailer.reply_message_notification(message, parent.user).deliver! if user.allow_email and parent
 		
-				else
-					team.users.each do |u|
-						unless u==user
-							puts "user is #{u.email}"
-							UserMailer.reply_message_notification(message, u).deliver! if u.allow_email and parent
-						end
-					end
-				end
+			# 	else
+			# 		team.users.each do |u|
+			# 			unless u==user
+			# 				puts "user is #{u.email}"
+			# 				UserMailer.reply_message_notification(message, u).deliver! if u.allow_email and parent
+			# 			end
+			# 		end
+			# 	end
 				
-			end
+			# end
 
 		rescue Exception => e
 			puts "Exception #{e.message}"
